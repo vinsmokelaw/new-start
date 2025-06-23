0aa6238ce67f0e1f14fe99c4970e217308c1d29d
vinsmokelaw <muchenutawanda4@gamail.com>
2025-06-23 14:33:17 +0200

    gear 5

--- src/Pages/HandyMan.jsx
+++ src/Pages/HandyMan.jsx
@@ -17,6 +17,7 @@ const LandingPage = () => {
       <Hero2 />
       <OurServices />
       <OurServices />
+      <Jobs />
       <Footer />
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,70 +1,61 @@
-import React from 'react'
-import {Link } from "react-router-dom"
-import "./Footer.css"
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import footerBg from '../../assets/pictures/footer.png';
+import buildLogo from '../../assets/pictures/buildLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className="flex items">
-            <div className="row">
-                <div className="col-sm-6 col-lg-3 col-md-3  centerItems">
-                    <div className="footerlogo">
-                    </div>
-                </div>
-                <div className="col-sm-6 col-lg-3 col-md-3  centerItems">
-                 <div className="footerlinks">
-                  <h4>Quick Links</h4>
-                    <Link>Home</Link>
-                    <Link>About</Link>
-                    <Link>Handyman</Link>
-                    <Link>Companies</Link>
-                 </div>
-                </div>
-                <div className="col-sm-6 col-lg-3 col-md-3  centerItems">
-              <div className="footerlink">
-                  <h4>Handyman</h4>
-                    <Link>Plumber</Link>
-                    <Link>Eletrician</Link>
-                    <Link>Builder</Link>
-                 </div>
-                </div>
-                <div className="col-sm-6 col-lg-3 col-md-3  centerItems">
-               <div className="footerlinks">
-                  <h4>Companies</h4>
-                    <Link>Construction Companies</Link>
-                    <Link>Electrical Specialists</Link>
-                    <Link>Plumber Specialists</Link>
-                    <Link>Builder Specialists</Link>
-                 </div>
-                </div>
+    <div className='footer' style={{ backgroundImage: `url(${footerBg})` }}>
+      <div className="flex items">
+        <div className="row">
+          <div className="col-sm-6 col-lg-3 col-md-3 centerItems">
+            <div className="footerlogo" style={{ backgroundImage: `url(${buildLogo})` }}></div>
+          </div>
+          <div className="col-sm-6 col-lg-3 col-md-3 centerItems">
+            <div className="footerlinks">
+              <h4>Quick Links</h4>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/handyman">Handyman</Link>
+              <Link to="/companies">Companies</Link>
             </div>
+          </div>
+          <div className="col-sm-6 col-lg-3 col-md-3 centerItems">
+            <div className="footerlink">
+              <h4>Handyman</h4>
+              <Link to="/handyman/plumber">Plumber</Link>
+              <Link to="/handyman/electrician">Electrician</Link>
+              <Link to="/handyman/builder">Builder</Link>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-3 col-md-3 centerItems">
+            <div className="footerlinks">
+              <h4>Companies</h4>
+              <Link to="/companies/construction">Construction</Link>
+              <Link to="/companies/electrical">Electrical</Link>
+              <Link to="/companies/plumber">Plumber</Link>
+              <Link to="/companies/builder">Builder</Link>
+            </div>
+          </div>
         </div>
-        <div className="copyright row">
-         <div className="col-md-4">
-           <p>www.buildlink@gmail.com</p>
-         </div>
-         <div className="col-md-4 socialsFooter">
-     <div className="socialIcon">
-      <p>🔗</p>
-       <p>linkedin</p>
-     </div>
-      <div className="socialIcon">
-      <p>🔗</p>
-       <p>instagram</p>
-     </div>
-     <div className="socialIcon">
-      <p>🔗</p>
-       <p>twitter</p>
-     </div>
-    <div className="socialIcon">
-      <p>🔗</p>
-       <p>facebook</p>
-     </div>
-         </div>
-         <div className="col-md-4"></div>
-        </div>
-      
-    </div>
-  )
-}
+      </div>
 
-export default Footer
+      <div className="copyright row">
+        <div className="col-md-4">
+          <p>www.buildlink@gmail.com</p>
+        </div>
+        <div className="col-md-4 socialsFooter">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
